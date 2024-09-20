4f2dca0826fb0551d85313e410fda2e2ad2d0509
angdan62 <angdan62@gmail.com>
2024-09-20 13:29:12 -0700

    a

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const mensajeQR = document.getElementById("mensajeqr");
 const imagenQR = document.getElementById("imagenQR");
 const imgQR = "qr.png";
-const imgtexto = "texto.jpg";
+const imgtexto = "mensaje.png";
 
 let qrToText = true;
 
