80249a4d91dffc9cf396c6924432ce7b7bc5a43f
angdan62 <angdan62@gmail.com>
2024-09-23 11:46:01 -0700

    ADD SPOTIFY ICON

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const mensajeQR = document.getElementById("mensajeqr");
 const imagenQR = document.getElementById("imagenQR");
 const imgQR = "qr.png";
 const imgtexto = "mensaje.png";
+const spotifyIcon = getElementById("spotifyIcon");
 
 let qrToText = true;
 
@@ -14,3 +15,7 @@ mensajeQR.addEventListener("click", ()=>{
     }
     qrToText = !qrToText;
 })
+
+// spotifyIcon.addEventListener("click", () =>{
+
+// })
