ee28b54fd5d0937feeb6aeefe94b26c9a2029722
angdan62 <angdan62@gmail.com>
2024-10-30 22:32:00 -0600

    SE AÑADEN ESTILOS A HOME, SE AÑADEN SECCIONES DE IMAGENES E INICIO

---
+++ scripts/home.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CONTROLES PARA MANEJO DE PANELES EN LA SECCION DE GALERIA
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
