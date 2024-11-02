aead6bf3a69b63a1dc8981ccf2863fb48cdfb72c
angdan62 <angdan62@gmail.com>
2024-11-01 22:45:13 -0600

    .

--- scripts/home.js
+++ scripts/home.js
@@ -23,19 +23,33 @@
 
 
 
-// CONTROLES PARA MANEJO DE PANELES EN LA SECCION DE GALERIA
+// CONTROLES PARA MANEJO DE PANELES EN LA SECCION DE GALERIA Y VIDEOS
+
 
 const panels = document.querySelectorAll('.panel')
+const panels2 = document.querySelectorAll('.panel2')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        removeActiveClasses()
+        removeActiveClasses(panels)
         panel.classList.add('active')
     })
 })
 
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
+panels2.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses(panels2)
+        panel.classList.add('active2')
+    })
+})
+
+
+function removeActiveClasses(panel) {
+    panel.forEach(p => {
+        if(p.classList.contains('active')){
+            p.classList.remove('active');
+        }else{
+            p.classList.remove('active2');
+        }
     })
 }
