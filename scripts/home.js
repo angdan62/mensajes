84697c6408d4b4fbd74b7d624e4c4cfaebfe7d9b
angdan62 <angdan62@gmail.com>
2024-11-05 18:55:56 -0600

    cambios en img. videos, etc

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,21 @@
+const videoList = document.querySelectorAll(".videoIcon");
+const videoShow = document.querySelector(".videoShow");
 
 
+console.log(videoList[0])
+let i=0;
+videoList.forEach(video => {
+    video.addEventListener("click", () =>{
+        playVideo();
+    })
+});
+
+
+function playVideo(){
+    videoShow.innerHTML=`<video class="videoPlay" width="100%" height="100%" controls>  
+                            <source src="../img/home/videos/vi_001.mp4" type="video/mp4">   
+                        </video> "`
+}
 
 
 
--- scripts/login.js
+++ scripts/login.js
@@ -6,13 +6,13 @@ const submitInput = document.querySelector(".login");
 const forgetPassword = document.querySelector(".forgetPassword");
 const alertMessage = document.querySelector(".alertMessage");
 // const keyUser = "e102f95572e59f8e8f4590e6dfd86f64d1dbd9b92ee9847e7875bedcecbe74d9";
-const keyUser = "ahsajsndkjhaskdaksdkashd";
+const keyUser = "3ea87a56da3844b420ec2925ae922bc731ec16a4fc44dcbeafdad49b0e61d39c";
 //CONTROL PARA CUANDO SE OLVIDÓ LA CONTRASEÑA
 
 forgetPassword.addEventListener("click", () =>{
     swal({
         title: "¿Te has olvidado de la fecha especial?",
-          text: "Fecha a 6 digitos, dd/mm/aa",
+          text: "Fecha a 6 digitos, ddmmaa",
           type: "warning",
           confirmButtonColor: '#3085d6',
           confirmButtonText: 'Volver a Intentarlo'
@@ -25,7 +25,7 @@ forgetPassword.addEventListener("click", () =>{
 submitInput.addEventListener("click", ()=>{
     let passwordUser = encryptSHA256(passwordInput.value);
     if(passwordUser == keyUser){
-        window.open("./pages/main.html", "_self");
+        window.open("./pages/home.html", "_self");
     }
     else{
         alertMessage.textContent = "Contraseña Incorrecta."
