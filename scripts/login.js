39958096af581819203068fbb572ea8d4e1dc43a
angdan62 <angdan62@gmail.com>
2024-12-06 13:54:33 -0600

    change password

--- scripts/login.js
+++ scripts/login.js
@@ -5,8 +5,8 @@ const passwordInput = document.querySelector(".password");
 const submitInput = document.querySelector(".login");
 const forgetPassword = document.querySelector(".forgetPassword");
 const alertMessage = document.querySelector(".alertMessage");
-// const keyUser = "e102f95572e59f8e8f4590e6dfd86f64d1dbd9b92ee9847e7875bedcecbe74d9";
-const keyUser = "3ea87a56da3844b420ec2925ae922bc731ec16a4fc44dcbeafdad49b0e61d39c";
+const keyUser = "e102f95572e59f8e8f4590e6dfd86f64d1dbd9b92ee9847e7875bedcecbe74d9";
+// const keyUser = "3ea87a56da3844b420ec2925ae922bc731ec16a4fc44dcbeafdad49b0e61d39c";
 //CONTROL PARA CUANDO SE OLVIDÓ LA CONTRASEÑA
 
 
