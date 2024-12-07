923af49efb3e0ab25f31aa9ba85ffe04768a2270
angdan62 <angdan62@gmail.com>
2024-12-07 00:34:40 -0600

    Se añade evento enter para login

--- scripts/login.js
+++ scripts/login.js
@@ -5,7 +5,7 @@ const passwordInput = document.querySelector(".password");
 const submitInput = document.querySelector(".login");
 const forgetPassword = document.querySelector(".forgetPassword");
 const alertMessage = document.querySelector(".alertMessage");
-const keyUser = "e102f95572e59f8e8f4590e6dfd86f64d1dbd9b92ee9847e7875bedcecbe74d9";
+const storedPasswordHash = "e102f95572e59f8e8f4590e6dfd86f64d1dbd9b92ee9847e7875bedcecbe74d9";
 // const keyUser = "3ea87a56da3844b420ec2925ae922bc731ec16a4fc44dcbeafdad49b0e61d39c";
 //CONTROL PARA CUANDO SE OLVIDÓ LA CONTRASEÑA
 
@@ -14,26 +14,31 @@ const keyUser = "e102f95572e59f8e8f4590e6dfd86f64d1dbd9b92ee9847e7875bedcecbe74d
 //CONTROL INICIAR SESIÓN
 
 submitInput.addEventListener("click", ()=>{
-    let passwordUser = encryptSHA256(passwordInput.value);
-    if(passwordUser == keyUser){
+    validatePassword(passwordInput.value);
+    
+})
+
+// Agregar evento al input
+passwordInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        validatePassword(passwordInput.value);
+        e.preventDefault();
+    
+    }
+});
+
+// Función para validar la contraseña
+function validatePassword(password){
+    let inputPasswordHash  = encryptSHA256(password);
+    if(inputPasswordHash  == storedPasswordHash){
         window.open("./pages/home.html", "_self");
     }
     else{
         alertMessage.textContent = "Contraseña Incorrecta."
         passwordInput.value="";
     }
-})
+}
 
-submitInput.addEventListener("keypress", ()=>{
-    if(e.key === "enter"){
-
-        alert("Enter message")
-    }
-})
-
-passwordInput.addEventListener("click", ()=>{
-    alertMessage.textContent="";
-})
 
 
 //FUNCION PARA ENCRIPTAR KEY
