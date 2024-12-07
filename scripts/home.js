0c6f9dcd8fefd04bea5dc09323e3a39ab0f3a545
angdan62 <angdan62@gmail.com>
2024-12-07 00:59:00 -0600

    Se añaden iconos y se depura codigo

--- scripts/home.js
+++ scripts/home.js
@@ -77,17 +77,11 @@ async function useSpotifyData() {
 }
 
 
-
-
-
-
-
-
-// CONTROLES PARA MANEJO DE PANELES EN LA SECCION DE GALERIA Y VIDEOS
+// CONTROLES PARA MANEJO DE PANELES EN LA SECCION DE GALERIA
 
 
 const panels = document.querySelectorAll('.panel')
-const panels2 = document.querySelectorAll('.panel2')
+
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
@@ -96,20 +90,10 @@ panels.forEach(panel => {
     })
 })
 
-panels2.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses(panels2)
-        panel.classList.add('active2')
-    })
-})
-
-
 function removeActiveClasses(panel) {
     panel.forEach(p => {
         if(p.classList.contains('active')){
             p.classList.remove('active');
-        }else{
-            p.classList.remove('active2');
         }
     })
 }
